b6d8ddf3fa76c317bbaff8d568e73c58dac57b24
dbanerji <deboshruti.banerji95@gmail.com>
2023-04-21 17:01:02 +0100

    Adds the bare Springboot Application

---
+++ src/main/java/com/SplitEasier/spliteasier/SpliteasierApplication.java
@@ -0,0 +1,13 @@
+package com.SplitEasier.spliteasier;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpliteasierApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpliteasierApplication.class, args);
+	}
+
+}
