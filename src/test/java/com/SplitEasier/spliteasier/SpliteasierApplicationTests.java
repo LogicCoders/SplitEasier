cdd93307e5aad9fe4d8801d181ce0a04e96cb357
dbanerji <deboshruti.banerji95@gmail.com>
2023-04-21 17:01:02 +0100

    Adds the bare Springboot Application

---
+++ src/test/java/com/SplitEasier/spliteasier/SpliteasierApplicationTests.java
@@ -0,0 +1,13 @@
+package com.SplitEasier.spliteasier;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpliteasierApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
